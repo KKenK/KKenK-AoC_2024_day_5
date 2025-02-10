544eeeda7ecb46ad6f5043f654ebd9690784d9cb
Kyle <kylekenrick55@gmail.com>
2025-02-10 23:32:23 +0000

    part_1 working solution committed.

---
+++ part_1.py
@@ -0,0 +1,81 @@
+import input_parser
+from collections import namedtuple
+
+Page = namedtuple("Page", ["number", "preceding_pages", "proceeding_pages"])
+
+class PageLine():
+    def __init__(self, line):
+        self.pages = self._make_line(line)
+
+    def _make_line(self, line):
+
+        pages = []
+
+        for i in range(len(line)):
+
+            proceeding_pages = line[i:]
+
+            proceeding_pages.pop(0)
+
+            pages.append(Page(number = line[i], preceding_pages= set(line[:i]), proceeding_pages = set(proceeding_pages)))
+
+        return pages
+
+    def print_line(self):
+        
+        for page in self.pages:
+            print(page.number, sep=",", end=" ")
+        
+        print(" ")
+
+class LinePageOrderAnalyser():
+
+    def __init__(self, page_ordering_rules_dict):
+        self._page_ordering_rules_dict = page_ordering_rules_dict
+
+    def is_line_correctly_ordered(self, line):
+
+        for page in line.pages:
+
+            if page.number not in self._page_ordering_rules_dict:
+                continue
+
+            if page.preceding_pages.intersection(self._page_ordering_rules_dict[page.number]):
+                return False
+
+            if not page.proceeding_pages.intersection(self._page_ordering_rules_dict[page.number]):
+                
+                if page == line.pages[-1]:
+                    continue
+
+                return False    
+        
+        return True
+
+class MiddlePageGetter():
+    @staticmethod
+    def get_middle_line(line):
+        
+        if len(line) % 2 != 0:
+            return line[len(line) // 2]
+        
+        return line[(len(line) // 2) - 1]
+
+if __name__ == "__main__":
+
+    input_parser = input_parser.InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_5\input.txt")
+
+    page_ordering_rules_dict, pages_to_produce = input_parser.parsed_input
+
+    line_page_order_analyser = LinePageOrderAnalyser(page_ordering_rules_dict)
+
+    middle_page_number_total = 0
+    
+    for line in pages_to_produce:
+        if not line_page_order_analyser.is_line_correctly_ordered(PageLine(line)):
+            continue
+
+        middle_page_number_total += MiddlePageGetter.get_middle_line(line)
+    
+    print(middle_page_number_total)
+    
