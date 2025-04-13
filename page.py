f53930d883ff12837955bee67224b8233e83a95c
Kyle <kylekenrick55@gmail.com>
2025-04-14 00:36:23 +0100

    Page class moved into its own module page.py

---
+++ page.py
@@ -0,0 +1,5 @@
+class Page():
+    def __init__(self, number, preceding_pages, proceeding_pages):
+        self.number =  number
+        self.preceding_pages = preceding_pages
+        self.proceeding_pages = proceeding_pages
