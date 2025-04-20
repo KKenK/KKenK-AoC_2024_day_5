0de272fd01d1a0e4fd6751098438d207f852e7fe
Kyle <kylekenrick55@gmail.com>
2025-04-20 03:09:45 +0100

    classes package created to contain input_parser, page_line and page classes.

---
+++ classes/input_parser.py
@@ -0,0 +1,28 @@
+class InputParser():
+
+    def __init__(self, input_path):
+
+        input = []
+
+        with open(input_path) as f:
+            input = f.read()
+
+        self.parsed_input = self._parse_file(input)
+
+    def _parse_file(self, input):
+
+        page_ordering_rules, pages_to_produce = input.split("\n\n")
+    
+        page_ordering_rules = [[int(y) for y in x.split("|")] for x in page_ordering_rules.split("\n")]
+
+        page_ordering_rules_dict = {x[0] : set([y[1] for y in page_ordering_rules if y[0] == x[0]]) for x in page_ordering_rules}             
+
+        pages_to_produce = [[int(y) for y in x.split(",")] for x in pages_to_produce.split("\n")]
+
+        return page_ordering_rules_dict, pages_to_produce
+
+if __name__ == "__main__":
+
+    input_parser = InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_5\test.txt")
+
+    print(input_parser.parsed_input)
---
+++ classes/page_line.py
@@ -0,0 +1,20 @@
+from . import page
+
+class PageLine():
+    def __init__(self, line):
+        self.pages = self._make(line)
+
+    def _make(self, line):
+
+        pages = []
+
+        for i in range(len(line)):
+
+            proceeding_pages = line[i:]
+
+            proceeding_pages.pop(0)
+
+            pages.append(page.Page(number = line[i], preceding_pages= set(line[:i]), proceeding_pages = set(proceeding_pages)))
+
+        return pages
+
