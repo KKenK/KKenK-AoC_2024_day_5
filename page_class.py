9c9ab8b259ad460d9cade4f1fd39161064dc3a00
Kyle <kylekenrick55@gmail.com>
2025-04-20 03:10:50 +0100

    classes moved into the class package removed.

--- page_class.py
+++
@@ -1,5 +0,0 @@
-class Page():
-    def __init__(self, number, preceding_pages, proceeding_pages):
-        self.number =  number
-        self.preceding_pages = preceding_pages
-        self.proceeding_pages = proceeding_pages
