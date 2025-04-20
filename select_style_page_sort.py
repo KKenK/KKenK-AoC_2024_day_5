2bbc14c6722bc09b6619827d2d6cca9fd5565d4a
Kyle <kylekenrick55@gmail.com>
2025-04-20 14:02:18 +0100

    select_style_page_sort.py: page_index variable renamed to current_page_index.

--- select_style_page_sort.py
+++ select_style_page_sort.py
@@ -4,14 +4,14 @@ def select_page_sort(page_ordering_rules_line_subdict, line):
 
     line_pages = [page.number for page in line.pages]
 
-    page_index = 0
+    current_page_index = 0
 
-    while page_index < len(line_pages):
+    while current_page_index < len(line_pages):
 
-        current_page = line_pages[page_index]
+        current_page = line_pages[current_page_index]
 
         if current_page not in page_ordering_rules_line_subdict:
-            page_index += 1
+            current_page_index += 1
             continue
         
         current_page_values= page_ordering_rules_line_subdict[current_page]
@@ -23,7 +23,7 @@ def select_page_sort(page_ordering_rules_line_subdict, line):
 
             value_index = line_pages.index(value_page_number)
  
-            if value_index > page_index:
+            if value_index > current_page_index:
                 continue
 
             pages_to_insert_after_current_page.append(line_pages[value_index])
@@ -31,10 +31,10 @@ def select_page_sort(page_ordering_rules_line_subdict, line):
             indexs_of_rule_violating_pages.append(value_index)
 
         if not indexs_of_rule_violating_pages:
-            page_index += 1
+            current_page_index += 1
             continue
 
-        line_pages = line_pages[:page_index + 1] + pages_to_insert_after_current_page + line_pages[page_index + 1:]
+        line_pages = line_pages[:current_page_index + 1] + pages_to_insert_after_current_page + line_pages[current_page_index + 1:]
 
         indexs_of_rule_violating_pages = sorted(indexs_of_rule_violating_pages, reverse=True)
 
@@ -42,7 +42,7 @@ def select_page_sort(page_ordering_rules_line_subdict, line):
              line_pages.pop(index)           
 
         
-        page_index -= len(pages_to_insert_after_current_page) + 1
+        current_page_index -= len(pages_to_insert_after_current_page) + 1
 
     line = page_line.PageLine(line_pages)
 
